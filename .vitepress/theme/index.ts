6cdc2d317fe260588d73563043073f4b4c5f3bd1
share121 <2854631158@qq.com>
2024-12-24 18:32:22 +0800

    feat(Layout.vue): 添加图片放大

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,19 @@
 import DefaultTheme from "vitepress/theme";
-import MyLayout from "./MyGiscus.vue";
+import MyLayout from "./Layout.vue";
+import mediumZoom from "medium-zoom";
+import { useRoute } from "vitepress";
+import { nextTick, onMounted, watch } from "vue";
 import "./custom.css";
 
 export default {
   extends: DefaultTheme,
   // 使用注入插槽的包装组件覆盖 Layout
   Layout: MyLayout,
+  setup() {
+    const route = useRoute();
+    const initRoom = () =>
+      mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+    onMounted(initRoom);
+    watch(() => route.path, () => nextTick(initRoom));
+  },
 };
